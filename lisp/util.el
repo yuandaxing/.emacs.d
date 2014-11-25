b4c7dfe5902a948eb5d6f30c4deb44c316f3dc30
yuandaxing <mryuan0@gmail.com>
2014-11-26 07:58:01 +0800

    add some changes

--- lisp/util.el
+++ lisp/util.el
@@ -10,12 +10,16 @@
 		 (and (fboundp x)                     ; does x name a function?
 			  (not (commandp (symbol-function x))) ; is it non-interactive?
 			  (subrp (symbol-function x))          ; is it built-in?
-			  (string-match name (system-name x))
+			  (string-match name (symbol-name x))
 			  (add-to-list 'funclist 
 						   (concat (symbol-name x) " - " (first-line (documentation x))
 								   "\n")))))
+	  (with-current-buffer (get-buffer-create "output")
 	  (dolist (item (sort funclist 'string<))
-		(insert item)))))
+		(insert item))))))
+
+
+
 
 
 
