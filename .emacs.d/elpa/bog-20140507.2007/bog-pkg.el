330574cf71897918531a2a3bf9e9e8fa78324feb
yuandaxing <mryuan0@gmail.com>
2014-11-25 21:50:56 +0800

    remove all

--- .emacs.d/elpa/bog-20140507.2007/bog-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "bog" "20140507.2007" "Extensions for research notes in Org mode" (quote ((dash "2.5.0"))))
--- elpa/request-20140316.417/request-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; request-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("request-pkg.el" "request.el") (21323
-;;;;;;  11544 652662 500000))
-
-;;;***
-
-(provide 'request-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; request-autoloads.el ends here
