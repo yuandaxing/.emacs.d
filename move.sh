2ce99918191f8d657a3514d626baaa8c28326cba
yuandaxing <mryuan0@gmail.com>
2014-11-25 23:33:24 +0800

    color support for echo

--- move.sh
+++ move.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 
 mkdir -vp ./lisp/ ~/.emacs.d/lisp/
-echo $1
-if [ "$1"=="commit" ]; then
-	echo "start to move commit files"
+if [ "$1" == "commit" ]; then
+	echo -e "\033[35mstart to move commit files\033[0m"
 	cp -vrf ~/.emacs.d/start-init.el ./
 	cp -vrf  ~/.emacs.d/lisp/* ./lisp/
-elif [ "$1"=="install" ]; then
-	echo "start to move install files"
+elif [ "$1" == "install" ]; then
+	echo -e "\033[35mstart to move install files\033[0m"
 	cp -vrf ./start-init.el ~/.emacs.d/ 
 	cp -vrf lisp/* ~/.emacs.d/lisp/
 else 
-    echo "error command"
-	echo "./move.sh [(commit)|(install)]"
+    echo -e "\033[33merror command\033[0m"
+	echo -e "\033[33m./move.sh [(commit)|(install)]\033[0m"
 fi
