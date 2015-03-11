38615d967de07f4571c66a69b25ae37d94fdb503
yuandaxing <mryuan0@gmail.com>
2015-03-11 10:08:08 +0800

    test

--- lisp/init-tramp.el
+++ lisp/init-tramp.el
@@ -1,7 +1,11 @@
 (require-package 'tramp)
+
 (require 'tramp)
-(setq tramp-default-method "scpx")
+
+(set-default 'tramp-default-method "scpx")
+
 (defun s1()
   (interactive)
-  (find-file "/yuandx@10.0.2.140:~/mv_code/trunk"))
+  (find-file "/yuandx@10.0.2.140:~/mv_code/trunk/"))
+
 (provide 'init-tramp)
