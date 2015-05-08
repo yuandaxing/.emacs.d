f7176bc6c6610577c087a4293780f8532729e5c0
yuandaxing <mryuan0@gmail.com>
2015-05-08 15:08:56 +0800

    change c-x c-m

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -142,7 +142,8 @@
 ;; Handy key bindings
 ;;----------------------------------------------------------------------------
 ;; To be able to M-x without meta
-(global-set-key (kbd "C-x C-m") 'execute-extended-command)
+(global-set-key (kbd "C-x C-m") 'smex)
+(global-set-key (kbd "C-c C-m") 'smex)
 
 ;; Vimmy alternatives to M-^ and C-u M-^
 (global-set-key (kbd "C-c j") 'join-line)
