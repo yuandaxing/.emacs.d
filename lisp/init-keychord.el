c8bece38d44580ee03ebfda071b219c8b7dbb095
yuandaxing <1179923349@qq.com>
2015-06-29 22:43:35 +0800

    all

--- lisp/init-keychord.el
+++ lisp/init-keychord.el
@@ -6,7 +6,7 @@
 (key-chord-define-global "jk" 'avy-goto-char)
 (key-chord-define-global "JJ" 'prelude-switch-to-previous-buffer)
 (key-chord-define-global "uu" 'undo-tree-visualize)
-(key-chord-define-global "xx" 'execute-extended-command)
+(key-chord-define-global "xx" 'smex)
 (key-chord-define-global "yy" 'browse-kill-ring)
 
 ;; (defvar key-chord-tips '("Press <jj> quickly to jump to the beginning of a visible word."
