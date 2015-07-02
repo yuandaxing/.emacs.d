3e17b7cb020e0d4f0b6c266e9dd85327b9dd785b
yuandaxing <mryuan0@gmail.com>
2015-07-02 14:00:59 +0800

    update scroll settings

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -432,5 +432,10 @@ point reaches the beginning or end of the buffer, stop there."
 ;
 
 (fset 'yes-or-no-p 'y-or-n-p)
+(setq redisplay-dont-pause t
+  scroll-margin 1
+  scroll-step 1
+  scroll-conservatively 10000
+  scroll-preserve-screen-position 1)
 ;----------------------------------------------------------------------------
 (provide 'init-editing-utils)
