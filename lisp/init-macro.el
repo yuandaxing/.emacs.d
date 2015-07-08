e9b4ece765abd79ce07db1ef86b821437d61124e
yuandaxing <mryuan0@gmail.com>
2015-07-08 10:19:37 +0800

    clear buffer

--- lisp/init-macro.el
+++ lisp/init-macro.el
@@ -1,4 +1,7 @@
 (fset 'move-brace-down
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("{
 " 0 "%d")) arg)))
+
+
+(fset 'clear-buff "\^xh\^x\^k")
 (provide 'init-macro)
