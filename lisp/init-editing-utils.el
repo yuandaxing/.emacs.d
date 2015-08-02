eade1ffecf80fd41da344da0368c831df7ff50c1
yuandaxing <mryuan0@gmail.com>
2015-08-02 11:57:57 -0400

    all

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -1,6 +1,8 @@
 (require-package 'unfill)
+(when (fboundp 'electric-pair-mode)
+  (electric-pair-mode))
 (when (eval-when-compile (version< "24.4" emacs-version))
-  (electric-indent-mode))
+  (electric-indent-mode 1))
 
 ;;----------------------------------------------------------------------------
 ;; Some basic preferences
