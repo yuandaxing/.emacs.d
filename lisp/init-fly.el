60bf13aafd25ba17e7c88d2923d8218dc4d89a6a
yuandaxing <mryuan0@gmail.com>
2015-09-17 08:48:02 -0400

    local update

--- lisp/init-fly.el
+++ lisp/init-fly.el
@@ -2,5 +2,6 @@
   :ensure t
   :config
   (add-hook 'after-init-hook #'global-flycheck-mode)
+  (add-hook 'c++-mode-hook (lambda () (setq flycheck-gcc-language-standard "c++11")))
   (diminish 'flycheck-mode))
 (provide 'init-fly)
