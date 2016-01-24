1a4257812cda7e6804564d1dd464965f1bec39f3
yuandaxing <mryuan0@gmail.com>
2016-01-24 20:38:15 +0800

    shell settings

---
+++ personal.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+### BEGIN INIT INFO
+# description: personal settings
+# Provides: functions
+# Required-Start:  personal.sh start 
+# Required-Stop: personal.sh stop 
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+### END INIT INFO
+start()
+{
+    if pgrep xflux
+    then
+        echo "start xflux alread"
+    else
+        xflux -l 31.19 -g 121.76 -k 5500
+    fi
+    setxkbmap -option ctrl:swapcaps
+}
+
+start 
+
