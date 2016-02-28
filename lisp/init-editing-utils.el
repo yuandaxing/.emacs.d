1d88c3db60e27919abb89913e4e358aeb88d4bdd
yuandaxing <mryuan0@gmail.com>
2016-02-28 12:25:54 +0800

    stages

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -275,7 +275,7 @@ With arg N, insert N newlines."
   :ensure t
   :config
   (progn 
-    (setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x 5" "C-c" "C-x n" "C-c h"))
+    (setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x 5" "C-c" "C-x n" "C-c h" "C-c p"))
     (guide-key-mode 1)
     (diminish 'guide-key-mode)))
 
