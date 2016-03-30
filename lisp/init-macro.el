190e469d7ebf7a999b0d67e06cdf603b94c7574a
yuandaxing <mryuan0@gmail.com>
2016-03-30 15:26:11 +0800

    insert diff numbers macro

--- lisp/init-macro.el
+++ lisp/init-macro.el
@@ -1,3 +1,7 @@
+;;; code:
 (fset 'clear-buff "\^xh\^x\^k")
 
+(fset 'diff-number-set
+      (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217830 23 25 24 111 134217788 19 25 return 24 111] 0 "%d")) arg)))
 (provide 'init-macro)
+;;;
