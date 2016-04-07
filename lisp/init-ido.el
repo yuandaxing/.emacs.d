dc52783a856ce3f6c9e6928d537bb68b94eedc9c
yuandaxing <mryuan0@gmail.com>
2016-04-07 23:58:12 +0800

    enable ido for projectile

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -1,8 +1,8 @@
-;; (use-package ido-vertical-mode
-;;                  :ensure t
-;;                  :config
-;;                  (progn
-;;                    (require 'ido-vertical-mode)
-;;                    (ido-vertical-mode 1)
-;;                    (setq ido-vertical-define-keys 'C-n-and-C-p-only)))
+(use-package ido-vertical-mode
+                 :ensure t
+                 :config
+                 (progn
+                   (require 'ido-vertical-mode)
+                   (ido-vertical-mode 1)
+                   (setq ido-vertical-define-keys 'C-n-and-C-p-only)))
 (provide 'init-ido)
