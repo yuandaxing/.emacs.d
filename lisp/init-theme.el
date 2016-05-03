7bf7721baa217e0f3ce16cfd7998d1b786402be2
yuandaxing <mryuan0@gmail.com>
2016-05-03 19:20:49 +0800

    faces

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -4,4 +4,10 @@
   (progn
     (require 'solarized-theme)
     (load-theme 'solarized-light t)))
+
+(add-hook 'shell-mode-hook
+               (lambda ()
+                (font-lock-add-keywords nil
+                                        '(("\\<\\(FIXME\\|TODO\\)" 1 font-lock-warning-face t))
+                                        '(("\\<\\(BUG\\)" 1 font-lock-keyword-face t)))))
 (provide 'init-theme)
