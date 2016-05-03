a0c4b0a443fec8e9d6130f1ee9b30d0ea36de1c3
yuandaxing <mryuan0@gmail.com>
2016-05-03 19:45:31 +0800

    add warnings

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -5,9 +5,11 @@
     (require 'solarized-theme)
     (load-theme 'solarized-light t)))
 
+
 (add-hook 'shell-mode-hook
                (lambda ()
                 (font-lock-add-keywords nil
-                                        '(("\\<\\(FIXME\\|TODO\\)" 1 font-lock-warning-face t))
-                                        '(("\\<\\(BUG\\)" 1 font-lock-keyword-face t)))))
+                 '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t)))))
+
+
 (provide 'init-theme)
