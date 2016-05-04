4802b83d904e165579f54d9d5a8c6e14ba0d3294
yuandaxing <mryuan0@gmail.com>
2016-05-04 19:15:36 +0800

    insert date

--- lisp/init-cpp.el
+++ lisp/init-cpp.el
@@ -146,4 +146,15 @@
   (async-shell-command
    (concatenate 'string "source /home/yuandx/rsa_keys/work_shortcut.sh; commit_syn " project)))
 
+(defun hh-insert-date (prefix)
+  "Insert the current date. With prefix-argument, use ISO format. With
+   two prefix arguments, write out the day and month name."
+  (interactive "P")
+  (let ((format (cond
+                 ((not prefix) "%d.%m.%Y")
+                 ((equal prefix '(4)) "%Y-%m-%d")
+                 ((equal prefix '(16)) "%A, %d. %B %Y")))
+        (system-time-locale "zh_CN"))
+    (insert (format-time-string format))))
+(global-set-key (kbd "C-c h d") 'hh-insert-date)
 (provide 'init-cpp)
