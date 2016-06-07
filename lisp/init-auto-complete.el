cf928ab77e187ee0fb5f73fb804e593061ced78e
yuandaxing <mryuan0@gmail.com>
2016-06-07 23:10:57 +0800

    all

--- lisp/init-auto-complete.el
+++ lisp/init-auto-complete.el
@@ -28,7 +28,7 @@
     (add-hook 'after-init-hook 'global-company-mode)
     (setq company-backends (delete 'company-semantic company-backends))
     (add-to-list 'company-backends 'company-c-headers)
-        (add-to-list 'company-backends 'company-abbrev)
+    ;(add-to-list 'company-backends 'company-abbrev)
     (define-key company-active-map (kbd "C-o") 'company-show-doc-buffer)
     (define-key company-active-map (kbd "C-l") 'company-show-location)
     (define-key company-active-map (kbd "M-o") 'company-show-doc-buffer)
@@ -39,8 +39,6 @@
     (add-to-list 'company-c-headers-path-system "/usr/include/c++/4.9/")
     (require 'cc-mode)
     ))
-
-
 (global-set-key (kbd  "C-:")  'company-complete)
 (defun tab-indent-or-complete ()
   (interactive)
@@ -52,7 +50,7 @@
             (company-complete-common)
           (indent-for-tab-command)))))
 
-(global-set-key [tab] 'tab-indent-or-complete)
+;(global-set-key [tab] 'tab-indent-or-complete)
 
 ;; (use-package auto-complete
 ;;   :ensure t
@@ -104,13 +102,13 @@
 ;; (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
 ;; Exclude very large buffers from dabbrev
-(defun hh-dabbrev-friend-buffer (other-buffer)
-  (< (buffer-size other-buffer) (* 1 1024 1024)))
+;; (defun hh-dabbrev-friend-buffer (other-buffer)
+;;   (< (buffer-size other-buffer) (* 1 1024 1024)))
 
-(setq dabbrev-friend-buffer-function 'hh-dabbrev-friend-buffer)
+;(setq dabbrev-friend-buffer-function 'hh-dabbrev-friend-buffer)
 
-(setq abbrev-file-name  (expand-file-name "abbrev/defs.el" user-emacs-directory))
-(setq-default abbrev-mode t)
-(setq save-abbrevs t)
-(quietly-read-abbrev-file)
+;(setq abbrev-file-name  (expand-file-name "abbrev/defs.el" user-emacs-directory))
+;(setq-default abbrev-mode t)
+;(setq save-abbrevs t)
+;(quietly-read-abbrev-file)
 (provide 'init-auto-complete)
