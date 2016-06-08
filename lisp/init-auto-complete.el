1b3631505bb753243e7f9f551d2d51ed70590c47
yuandaxing <mryuan0@gmail.com>
2016-06-08 21:53:58 +0800

    delete auto complete

--- lisp/init-auto-complete.el
+++ lisp/init-auto-complete.el
@@ -15,7 +15,6 @@
       (if (looking-at "\\.") t
         (backward-char 1)
         (if (looking-at "->") t nil)))))
-
 (defun do-yas-expand ()
   (let ((yas-expand 'return-nil))
     (yas-expand)))
@@ -50,56 +49,8 @@
             (company-complete-common)
           (indent-for-tab-command)))))
 
-                                        ;(global-set-key [tab] 'tab-indent-or-complete)
+(global-set-key [tab] 'tab-indent-or-complete)
 
-;; (use-package auto-complete
-;;   :ensure t
-;;   :config
-;;   (progn
-;;     (require 'auto-complete-config)
-;;     (global-auto-complete-mode t)
-;;     (diminish 'auto-complete-mode)
-;;     (setq-default ac-expand-on-auto-complete nil)
-;;     (setq-default ac-dwim nil) ; To get pop-ups with docs even if a word is uniquely completed
-;;     (setq tab-always-indent 'complete)  ;; use 't when auto-complete is disabled
-;;     (add-to-list 'completion-styles 'initials t)
-;;     (setq completion-cycle-threshold 5)
-;;     (setq c-tab-always-indent nil
-;;           c-insert-tab-function 'indent-for-tab-command)
-;;     (set-default 'ac-sources
-;;                  '(ac-source-imenu
-;;                    ac-source-dictionary
-;;                    ac-source-yasnippet
-;;                    ac-source-words-in-buffer
-;;                    ac-source-words-in-same-mode-buffers
-;;                    ac-source-words-in-all-buffer))
-;;     (dolist (mode '(magit-log-edit-mode
-;;                     log-edit-mode org-mode text-mode haml-mode
-;;                     git-commit-mode
-;;                     sass-mode yaml-mode csv-mode espresso-mode haskell-mode
-;;                     html-mode nxml-mode sh-mode smarty-mode clojure-mode
-;;                     lisp-mode textile-mode markdown-mode tuareg-mode
-;;                     js3-mode css-mode less-css-mode sql-mode
-;;                     sql-interactive-mode
-;;                     inferior-emacs-lisp-mode))
-;;       (add-to-list 'ac-modes mode))
-;;     ))
-
-;; (defun hh-auto-complete-at-point ()
-;;   (when (and (not (minibufferp))
-;; 	     (fboundp 'auto-complete-mode)
-;; 	     auto-complete-mode)
-;;     (auto-complete)))
-
-(defun hh-never-indent ()
-  (set (make-local-variable 'indent-line-function) (lambda () 'noindent)))
-
-;; (defun set-auto-complete-as-completion-at-point-function ()
-;;   (setq completion-at-point-functions
-;;         (cons 'hh-auto-complete-at-point
-;;               (remove 'hh-auto-complete-at-point completion-at-point-functions))))
-
-(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
 ;; Exclude very large buffers from dabbrev
 (defun hh-dabbrev-friend-buffer (other-buffer)
