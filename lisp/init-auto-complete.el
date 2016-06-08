c265b9ff8052c81b02d177007680ab780dd2bd22
yuandaxing <mryuan0@gmail.com>
2016-06-08 11:41:25 +0800

    update complete

--- lisp/init-auto-complete.el
+++ lisp/init-auto-complete.el
@@ -8,17 +8,17 @@
                  (expand-file-name "snippets" user-emacs-directory))
     (setq yas-wrap-around-region t)
     (diminish 'yas-minor-mode)))
-  (defun check-expansion ()
-    (save-excursion
-      (if (looking-at "\\_>") t
+(defun check-expansion ()
+  (save-excursion
+    (if (looking-at "\\_>") t
+      (backward-char 1)
+      (if (looking-at "\\.") t
         (backward-char 1)
-        (if (looking-at "\\.") t
-          (backward-char 1)
-          (if (looking-at "->") t nil)))))
+        (if (looking-at "->") t nil)))))
 
-  (defun do-yas-expand ()
-    (let ((yas-expand 'return-nil))
-      (yas-expand)))
+(defun do-yas-expand ()
+  (let ((yas-expand 'return-nil))
+    (yas-expand)))
 (use-package company-c-headers
   :ensure t)
 (use-package company
@@ -28,7 +28,7 @@
     (add-hook 'after-init-hook 'global-company-mode)
     (setq company-backends (delete 'company-semantic company-backends))
     (add-to-list 'company-backends 'company-c-headers)
-    ;(add-to-list 'company-backends 'company-abbrev)
+    (add-to-list 'company-backends 'company-abbrev)
     (define-key company-active-map (kbd "C-o") 'company-show-doc-buffer)
     (define-key company-active-map (kbd "C-l") 'company-show-location)
     (define-key company-active-map (kbd "M-o") 'company-show-doc-buffer)
@@ -50,7 +50,7 @@
             (company-complete-common)
           (indent-for-tab-command)))))
 
-;(global-set-key [tab] 'tab-indent-or-complete)
+                                        ;(global-set-key [tab] 'tab-indent-or-complete)
 
 ;; (use-package auto-complete
 ;;   :ensure t
@@ -99,13 +99,13 @@
 ;;         (cons 'hh-auto-complete-at-point
 ;;               (remove 'hh-auto-complete-at-point completion-at-point-functions))))
 
-;; (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
 ;; Exclude very large buffers from dabbrev
-;; (defun hh-dabbrev-friend-buffer (other-buffer)
-;;   (< (buffer-size other-buffer) (* 1 1024 1024)))
+(defun hh-dabbrev-friend-buffer (other-buffer)
+  (< (buffer-size other-buffer) (* 1 1024 1024)))
 
-;(setq dabbrev-friend-buffer-function 'hh-dabbrev-friend-buffer)
+(setq dabbrev-friend-buffer-function 'hh-dabbrev-friend-buffer)
 
 (setq abbrev-file-name  (expand-file-name "abbrev/defs.el" user-emacs-directory))
 (setq-default abbrev-mode t)
