ea90c3dd13455de6f41ce78638de812f1bb67b83
yuandaxing <mryuan0@gmail.com>
2016-09-01 17:21:59 +0800

    update

--- lisp/init-keychord.el
+++ lisp/init-keychord.el
@@ -13,7 +13,7 @@
          ("C-c h g" . helm-projectile-ag)
          ("C-c h a" . ff-get-other-file)
          ("C-c h b" . helm-projectile-ag)
-     ))
+           ))
 (use-package hydra
   :ensure t)
 
