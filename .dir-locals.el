165fac243217f3b24de17da4b48ea1f1d5b6011f
yuandaxing <mryuan0@gmail.com>
2016-09-03 14:28:28 +0800

    update .gitignore

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode
+  (helm-ag-base-command . "ag --nocolor --nogroup -U")
+  (helm-ag-use-grep-ignore-list . "elpa")
+  (helm-grep-ignored-directories . "elpa")))
