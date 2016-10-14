65e04996e83ad146f66f3634659822ea10e2bb08
dxyuan <daxing.yuan@yitu-inc.com>
2016-10-14 11:36:48 +0800

    all

---
+++ lisp/init-shell.el
@@ -0,0 +1,30 @@
+(require 'shell)
+(defun comint-write-history-on-exit (process event)
+  (comint-write-input-ring)
+  (let ((buf (process-buffer process)))
+    (when (buffer-live-p buf)
+      (with-current-buffer buf
+        (insert (format "\nProcess %s %s" process event))))))
+
+(defun turn-on-comint-history ()
+  (let ((process (get-buffer-process (current-buffer))))
+    (when process
+      (setq comint-input-ring-file-name
+            (format "~/.emacs.d/inferior-%s-history"
+                    (process-name process)))
+      (comint-read-input-ring)
+      (set-process-sentinel process
+                            #'comint-write-history-on-exit))))
+(defun mapc-buffers (fn)
+  (mapc (lambda (buffer)
+          (with-current-buffer buffer
+            (funcall fn)))
+        (buffer-list)))
+
+(defun comint-write-input-ring-all-buffers ()
+  (mapc-buffers 'comint-write-input-ring))
+(add-hook 'kill-emacs-hook 'comint-write-input-ring-all-buffers)
+(add-hook 'shell-mode-hook #'(lambda ()
+                               (add-hook 'kill-buffer-hook 'comint-write-input-ring :local t)))
+(add-hook 'shell-mode-hook 'turn-on-comint-history)
+(provide 'init-shell)
