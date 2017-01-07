f035c28b64613e234946edd7ee3b6e6b37a63cca
dxyuan <daxing.yuan@yitu-inc.com>
2017-01-07 17:49:39 +0800

    add memorized make

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -15,15 +15,13 @@
       org-fast-tag-selection-single-key 'expert
       org-html-validation-link nil
       org-export-kill-product-buffer-when-displayed t
-      org-tags-column 80
-      org-src-fontify-natively t)
+      org-tags-column 80)
 ; Refile targets include this file and any file contributing to the agenda - up to 5 levels deep
 (setq org-refile-targets (quote ((nil :maxlevel . 5) (org-agenda-files :maxlevel . 5))))
 ; Targets start with the file name - allows creating level 1 tasks
 (setq org-refile-use-outline-path (quote file))
 ; Targets complete in steps so we start with filename, TAB shows the next level of targets etc
 (setq org-outline-path-complete-in-steps t)
-
 
 (setq org-todo-keywords
       (quote ((sequence "TODO(t)" "STARTED(s)" "|" "DONE(d!/!)")
