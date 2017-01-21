2285cf1a1404ef23cacb65e23b2fcd6cac1b2db0
dxyuan <daxing.yuan@yitu-inc.com>
2017-01-21 16:43:02 +0800

    add web restclient mode

---
+++ lisp/init-php.el
@@ -0,0 +1,8 @@
+(use-package php-mode
+  :ensure t
+  :config
+  (progn
+    ))
+(use-package restclient
+  :ensure t)
+(provide 'init-php)
--- lisp/init-web.el
+++ lisp/init-web.el
@@ -1,8 +1,17 @@
-(use-package django-mode
+;; (use-package django-mode
+;;   :ensure t
+;;   :config
+;;   (progn
+;;     (add-to-list 'auto-mode-alist '("\\.html$" . django-html-mode))
+;;     ))
+
+(use-package web-mode
   :ensure t
   :config
-  (progn
-    (add-to-list 'auto-mode-alist '("\\.html$" . django-html-mode))
-    ))
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html\\.twig\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  )
 
 (provide 'init-web)
