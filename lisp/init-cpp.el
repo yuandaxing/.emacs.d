9a5c140a75a90e3cdcfa741b3917af3f64c189b0
dxyuan <daxing.yuan@yitu-inc.com>
2017-01-21 21:29:10 +0800

    update

--- lisp/init-cpp.el
+++ lisp/init-cpp.el
@@ -145,6 +145,7 @@
     ("python" "~/Dropbox/code-snippet/python/")
     ("snippet"  "~/Dropbox/code-snippet/")
     ("python"  "~/Dropbox/code-snippet/python/")
+    ("php"  "~/Dropbox/code-snippet/php/")
     ("shell"  "~/Dropbox/code-snippet/shell/")))
 
 (defun search-code-snippet (snippet)
