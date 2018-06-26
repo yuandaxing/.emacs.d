85c49a5c6edae5fe9f46fa79713b8b77a7710057
yuandaxing <mryuan0@gmail.com>
2018-06-26 11:11:00 +0800

    comment smart-mode-line

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -346,29 +346,29 @@ With a prefix ARG open line above the current line."
   (let ((comint-buffer-maximum-size 0))
     (comint-truncate-buffer)))
 
-(use-package smart-mode-line
-  :ensure t
-  :config
-  (progn
-    (setq sml/no-confirm-load-theme t)
-    (setq-default
-     mode-line-format
-     '("%e"
-       mode-line-front-space
-       mode-line-mule-info
-       mode-line-client
-       mode-line-modified
-       mode-line-remote
-       mode-line-frame-identification
-       mode-line-buffer-identification
-       "   "
-       mode-line-position
-       (vc-mode vc-mode)
-       "  "
-       mode-line-modes
-       mode-line-misc-info
-       mode-line-end-spaces))
-    (smart-mode-line-enable)))
+;; (use-package smart-mode-line
+;;   :ensure t
+;;   :config
+;;   (progn
+;;     (setq sml/no-confirm-load-theme t)
+;;     (setq-default
+;;      mode-line-format
+;;      '("%e"
+;;        mode-line-front-space
+;;        mode-line-mule-info
+;;        mode-line-client
+;;        mode-line-modified
+;;        mode-line-remote
+;;        mode-line-frame-identification
+;;        mode-line-buffer-identification
+;;        "   "
+;;        mode-line-position
+;;        (vc-mode vc-mode)
+;;        "  "
+;;        mode-line-modes
+;;        mode-line-misc-info
+;;        mode-line-end-spaces))
+;;     (smart-mode-line-enable)))
 
 (defun hh-copy-file-name-to-clipboard (arg)
   "Copy the current buffer file name to the clipboard."
